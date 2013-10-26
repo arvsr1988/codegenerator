afa889b9b6cbce8464e3a3664cb6bcb69020cb91
arvind <arvsr2002@gmail.com>
2013-10-26 22:18:45 +0530

    using tokenizer instead of splitting strings

--- CodeGenerator/src/com/arvind/codegenerator/Main.java
+++ CodeGenerator/src/com/arvind/codegenerator/Main.java
@@ -1,6 +1,12 @@
 package com.arvind.codegenerator;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -9,6 +15,8 @@ public class Main {
 		
 	public static void main(String[] args){
 		
+		InputStream modelIn = null;
+		
 		try {
 			
 		String inputFromFile = getInputFromFile();
@@ -16,17 +24,27 @@ public class Main {
 		if(StringUtils.isEmpty(inputFromFile)){
 			return;
 		}
-		String[] inputStringWords = inputFromFile.toLowerCase().split(" ");
 		
+		modelIn = new FileInputStream("resources/en-token.bin");
+		TokenizerModel model = new TokenizerModel(modelIn);
+		Tokenizer tokenizer = new TokenizerME(model);
+		String[] inputStringWords  = tokenizer.tokenize(inputFromFile);		
 		String[] tags = POSTagger.getTags(inputStringWords);
 		ClassGenerator.createClasses(inputStringWords, tags);
 		
 		} catch (Exception ex){
 			ex.printStackTrace();
-		}
+		} finally {
+			  if (modelIn != null) {
+				    try { modelIn.close(); }
+				    catch (Exception e) {}
+				  }
+				}
+		
 	}
 
 	private static String getInputFromFile() throws Exception {
+		//TODO : move this to a property file
 		return FileUtils.readFileToString(new File("/home/arvind/Desktop/input")).replaceAll("\n", " ");
 	}
 }
